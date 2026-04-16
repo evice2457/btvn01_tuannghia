440d03f2b7501f5d662e07a9d9dbd12ab74b0449
TuanNghia <trantuannghia2457@gmail.com>
2026-04-16 09:05:46 +0000

    change launch file

--- launch/distance_warning.launch.py
+++ launch/distance_warning.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import  DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
@@ -13,53 +13,18 @@ def generate_launch_description():
 
     return LaunchDescription([
         threshold_arg,
-        Node(
-            package='distance_warning',
-            executable='distance_publisher',
-            name='distance_publisher'
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_publisher_qos',
-            name='distance_publisher_qos'
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_listener',
-            name='distance_listener',
-            parameters=[{'threshold': threshold}]
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_listener_qos',
-            name='distance_listener_qos',
-            parameters=[{'threshold': threshold}]
-        ),
-        Node(
-            package='distance_warning',
-            executable='set_threshold_service',
-            name='set_threshold_service',
-            parameters=[{'threshold': threshold}]
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_action_server',
-            name='distance_action_server',
-            parameters=[{'threshold': threshold}]
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_action_client',
-            name='distance_action_client'
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_tf_broadcaster',
-            name='distance_tf_broadcaster'
-        ),
-        Node(
-            package='distance_warning',
-            executable='distance_tf_listener',
-            name='distance_tf_listener'
-        ),
+        
+        # Nhóm Node từ BTVN_01
+        Node(package='distance_warning', executable='distance_publisher', name='distance_publisher', output='screen'),
+        Node(package='distance_warning', executable='distance_listener', name='distance_listener', output='screen', parameters=[{'threshold': threshold}]),
+        Node(package='distance_warning', executable='set_threshold_service', name='set_threshold_service', output='screen', parameters=[{'threshold': threshold}]),
+        Node(package='distance_warning', executable='distance_action_server', name='distance_action_server', output='screen', parameters=[{'threshold': threshold}]),
+        Node(package='distance_warning', executable='distance_action_client', name='distance_action_client', output='screen'),
+
+        # Nhóm Node từ BTVN_02 (TF2 & QoS)
+        Node(package='distance_warning', executable='distance_publisher_qos', name='distance_publisher_qos', output='screen'),
+        Node(package='distance_warning', executable='distance_listener_qos', name='distance_listener_qos', output='screen', parameters=[{'threshold': threshold}]),
+        Node(package='distance_warning', executable='distance_tf_broadcaster', name='distance_tf_broadcaster', output='screen'),
+        Node(package='distance_warning', executable='distance_tf_listener', name='distance_tf_listener', output='screen', 
+             parameters=[{'tf_threshold': 1.0}]) # Thêm parameter cho TF listener
     ])
